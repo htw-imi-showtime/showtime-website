65c2c8414d92ade941c1ef78e53bcbdb1ae0f3a0
Barne Kleinen <barne.kleinen@htw-berlin.de>
2023-07-25 17:05:39 +0200

    archive back to root / adapt gh actions

--- bin/hugo-temp.sh
+++ bin/hugo-temp.sh
@@ -7,4 +7,3 @@ cp -r resources hugo-temp
 cp -r themes hugo-temp
 mv hugo-temp/content/archive/* hugo-temp/content/
 rm -rf hugo-temp/content/archive
-hugo --cleanDestinationDir --source hugo-temp --buildDrafts -p 1314 --environment production server
