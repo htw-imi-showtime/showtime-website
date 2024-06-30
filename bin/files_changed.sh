d9c1c88e4c9ffdb0eca338e1d9743d13f32c3a67
Barne Kleinen <barne.kleinen@htw-berlin.de>
2024-06-30 22:25:10 +0200

    cleanup and hugo action update

--- bin/files_changed.sh
+++ bin/files_changed.sh
@@ -14,20 +14,18 @@ echo "-----------------------"
 echo "project id: $PID"
 RE="content/ss24.*$PID"
 OK=$(echo $DIFF | tr ' ' '\n' | grep "$RE" || echo "")
+OK_SEP=$(echo $OK | tr ' ' ':')
+
 echo "-----  OK nl-----"
 echo  $OK | tr ' ' '\n'
-echo "-----  OK sep -----"
-OK_SEP=$(echo $OK | tr ' ' ':')
-echo  $OK_SEP 
+# echo "-----  OK sep -----"
+# echo  $OK_SEP 
+
 NOK=$(echo $DIFF | tr ' ' '\n' | grep -v "$RE" || echo "") 
+NOK_SEP=$(echo $NOK | tr ' ' ':')
 echo "----- NOK nl-----" 
 echo $NOK | tr ' ' '\n'
-echo "-----  NOK sep -----"
-NOK_SEP=$(echo $NOK | tr ' ' ':')
-echo  $NOK_SEP 
-echo "----- ---- -----" 
-echo "files_ok=$OK_SEP"
-echo "files_not_ok=$NOK_SEP"
-echo  $GITHUB_OUTPUT
+# echo "-----  NOK sep -----"
+# echo  $NOK_SEP 
 echo "files_ok=$OK_SEP" >> $GITHUB_OUTPUT
 echo "files_not_ok=$NOK_SEP" >> $GITHUB_OUTPUT
