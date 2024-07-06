ea2888fad6a1147f9618d866ea63d55e82f80aac
Barne Kleinen <barne.kleinen@htw-berlin.de>
2024-07-06 13:34:13 +0200

    commit at 13:34:13

---
+++ bin/check-project-update-pr/disk-usage.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+PID=$1
+SIZE=$(du -nks content/ss24/**/$PID-* | cut -f -1)
+LIMIT=500
+
+if [ "$SIZE" -le "$LIMIT" ]
+then 
+    echo "Size $SIZE is below limit of $LIMIT - ok"
+else
+    echo "Size $SIZE is over limit of $LIMIT - project folder's disk usage is too high!"
+    exit 12
+fi
