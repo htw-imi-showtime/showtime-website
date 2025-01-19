564a1520f836c6627c08ce4b4a9059f8f9670558
Barne Kleinen <barne.kleinen@htw-berlin.de>
2025-01-19 23:24:14 +0100

    allow 4th character in file name extensions

--- bin/check-project-update-pr/check-file-names.sh
+++ bin/check-project-update-pr/check-file-names.sh
@@ -3,15 +3,17 @@ FILES=$1
 
 MISSMATCHES=""
 
-file_pattern="^[a-z][^A-Z]*\.([a-z]{2}[a-z]?)$"
+file_pattern="^[a-z][^A-Z]*\.([a-z]{2,4})$"
 no_spaces=".*__THERE_SHOULD_BE_NO_SPACES_IN_FILENAMES__.*"
-echo "---- check file names $FILES"
+echo "---- checking file names:"
+echo "$FILES"
+echo "-----------"
 for fn in $FILES; do
     # echo "###$fn####"
     #echo "$fn  =~ $file_pattern"
     if ! [[ $fn  =~ $file_pattern ]] 
     then
-    >&2 echo "$fn does not match the pattern '$file_pattern'"
+    >&2 echo "-- $fn does not match the pattern '$file_pattern'"
     MISSMATCHES="$MISSMATCHES $fn"
     fi
 
@@ -26,7 +28,10 @@ done
 
 
 if [ ! -z  "$MISSMATCHES" ]; then
-  >&2 echo "ERROR: not all files match the file name convention"
+  >&2 echo "-----"
+  >&2 echo "  ERROR: not all files match the file name conventions"
   exit 18
+else
+  echo "  All filenames match the file name conventions!"
 fi
 
